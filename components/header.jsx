dbdaa48edc2876326ede228d25cc31bf4051073f
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-06-24 09:28:39 -0400

    storing user in db

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,30 @@
+"use client";
+
 import React from "react";
 import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useStoreUser } from "@/hooks/use-store-user";
+import BarLoader from "react-spinners/BarLoader";
 
 const Header = () => { 
+
+  const { isLoading } = useStoreUser();
+
   return ( 
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
+      <nav>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </nav>
+
+      {isLoading && (
+        <BarLoader width={"100%"} color="#36d7b7" />
+      )}
+    </header>
   )
 }
 
