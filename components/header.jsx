b7b07c5697bec3e00ab5f797baf86b85f45bdbaf
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-12-12 21:10:00 -0500

    fixes to frontend + updated info for deployment

--- components/header.jsx
+++ components/header.jsx
@@ -30,12 +30,12 @@ const Header = () => {
         </Link>
         {path==="/" && (
         <div className="hidden md:flex items-center gap-6">
-          <Link href="#feature" 
+          <Link href="#features" 
           className="text-sm font-medium hover:text-green-600 transition"
           >
             Features
           </Link>
-          <Link href="#feature" 
+          <Link href="#how-it-works" 
           className="text-sm font-medium hover:text-green-600 transition"
           >
             How It Works
